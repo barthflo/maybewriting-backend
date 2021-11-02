3a8d2247bb54973d93c2a8f4584c0425ece3b97e
barthflo <flrnt.barth@gmail.com>
2021-11-02 13:11:49 +0100

    renamed root folder

--- config/database.js
+++ config/database.js
@@ -1,18 +1,18 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'mysql',
-        host: env('DATABASE_HOST', '162.241.253.75'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'flobarth_francesco'),
-        username: env('DATABASE_USERNAME', 'flobarth_admin'),
-        password: env('DATABASE_PASSWORD', '277506Flo'),
-        ssl: env.bool('DATABASE_SSL', false),
-      },
-      options: {}
-    },
-  },
+	defaultConnection: 'default',
+	connections: {
+		default: {
+			connector: 'bookshelf',
+			settings: {
+				client: 'mysql',
+				host: env('DATABASE_HOST'),
+				port: env.int('DATABASE_PORT'),
+				database: env('DATABASE_NAME'),
+				username: env('DATABASE_USERNAME'),
+				password: env('DATABASE_PASSWORD'),
+				ssl: env.bool('DATABASE_SSL'),
+			},
+			options: {},
+		},
+	},
 });
